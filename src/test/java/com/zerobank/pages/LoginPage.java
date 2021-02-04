68403008e2db1a0fcb2f6c553b18fcdf39d04d38
Yusuf KUCUKVATAN <yusuf@kucukvatan.com>
2021-02-04 18:03:20 -0500

    src added

---
+++ src/test/java/com/zerobank/pages/LoginPage.java
@@ -0,0 +1,63 @@
+package com.zerobank.pages;
+
+import com.zerobank.utilities.BrowserUtils;
+import com.zerobank.utilities.ConfigurationReader;
+import com.zerobank.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginPage extends PageBase{
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(id="signin_button")
+    public WebElement signinButton;
+
+    @FindBy(id="user_login")
+    public WebElement username;
+
+    @FindBy(id="user_password")
+    public WebElement password;
+
+    @FindBy(css = "input[name=\"submit\"]")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//h3[contains(text(),\"Troubles\")]")
+    public WebElement troubleMessage;
+
+    @FindBy(css = ".alert.alert-error")
+    public WebElement errorMessage;
+
+    public void openUrl(){
+        Driver.get().get(ConfigurationReader.get("url"));
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public void login(String username, String password) {
+        signinButton.click();
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+        submitButton.click();
+    }
+
+    public boolean verifyAccountSummary(){
+        String expectedTitle="Zero - Account Summary";
+        String actualTitle=Driver.get().getTitle();
+        boolean verification=expectedTitle.equals(actualTitle);
+        return verification;
+    }
+    public boolean verifyTroubleMessageDisplayed(){
+        return troubleMessage.isDisplayed();
+    }
+
+    public boolean verifyErrorMessageDisplayed(){
+        return errorMessage.isDisplayed();
+    }
+
+}
---
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package com.zerobank.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions (
+        features = {"src/test/resources/features"},
+        glue = {"/com/zerobank/step_definitions"},
+        plugin = {"pretty", "html:target/default-cucumber-reports", "json:target/cucumber.json", "rerun:target/rerun.txt"},
+        monochrome = true,
+        dryRun = false,
+        tags = "@login"
+)
+public class CukesRunner {
+}
